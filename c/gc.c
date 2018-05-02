15dcb52acd8e374e66e5a623d25147e20b274abc
SongWei <leo_songwei@outlook.com>
2018-05-02 19:01:01 +1000

    function

--- c/gc.c
+++ c/gc.c
@@ -17,10 +17,10 @@ int alloc_CONS(mem_info* mem)
 		obj* head = (obj*)(mem->cursor + (size_t)mem->ptr);
 		head->type = CONS;
 		head->copied = 0;
-		size_t* offset =
+		size_t* ptrs =
 			(size_t*)((size_t)head + sizeof(obj));
-		offset[0] = (size_t)symbol_nil;
-		offset[1] = (size_t)symbol_nil;
+		ptrs[0] = (size_t)symbol_nil;
+		ptrs[1] = (size_t)symbol_nil;
 		mem->cursor += length_CONS();
 		return 0;
 	}else{
@@ -94,14 +94,14 @@ int alloc_SYMBOL(mem_info* mem, size_t length)
 size_t length_ARRAY(size_t length)
 {
 	return sizeof(obj) + sizeof(size_t)
-		+ sizeof(uint32_t) * length;
+		+ sizeof(size_t) * length;
 }
 
 int alloc_ARRAY(mem_info* mem, size_t length)
 {
 	if(mem->cursor + length_ARRAY(length) < mem->len){
 		obj* head = (obj*)(mem->cursor + (size_t)mem->ptr);
-		head->type = SYMBOL;
+		head->type = ARRAY;
 		head->copied = 0;
 		size_t* len = (size_t*)((size_t)head + sizeof(obj));
 		*len = length;
@@ -116,3 +116,30 @@ int alloc_ARRAY(mem_info* mem, size_t length)
 	}
 }
 
+size_t length_FUNCTION(size_t binary_length)
+{
+	return sizeof(obj) + sizeof(char) + sizeof(size_t)
+		+ binary_length;
+}
+
+int alloc_FUNCTION(mem_info* mem, size_t binary_length)
+{
+	if(mem->cursor + length_FUNCTION(binary_length) < mem->len){
+		obj* head = (obj*)(mem->cursor + (size_t)mem->ptr);
+		head->type = ARRAY;
+		head->copied = 0;
+		char* arg_len = (char*)((size_t)head + sizeof(obj));
+		*arg_len = 0;
+		size_t* blen = (size_t*)((size_t)arg_len
+					 + sizeof(char));
+		*blen = binary_length;
+		char* func_body = (char*)((size_t)blen
+					  + sizeof(size_t));
+		for(size_t i=0; i<binary_length; i++){
+			func_body[i] = 0;
+		}
+		mem->cursor += length_FUNCTION(binary_length);
+	}else{
+		return -1;
+	}
+}
